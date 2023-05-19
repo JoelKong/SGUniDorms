e9fb17ff90b478e5090f4ae8fc40f96571dacc4c
Joel Kong <joelkong2001@gmail.com>
2023-05-19 17:45:54 +0800

    feat: added logo

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,6 +1,12 @@
+import styles from "@/styles/navigation.module.css";
 import { FiLogIn } from "react-icons/fi";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Navigation() {
+  const [activeElement, setActiveElement] = useState<String | null>(null);
+
+
   return (
     <nav className="w-full border-b">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
@@ -8,27 +14,32 @@ export default function Navigation() {
           <div className="flex items-center justify-between w-full">
             <div className="flex-shrink-0">
               <a href="/" className="text-white text-lg font-semibold">
-                SGUniDorms
+                <div className="flex items-center">
+                  <Image
+                    src="/sgdorm.png"
+                    alt="SGUniDorm"
+                    width="40"
+                    height="40"
+                  />
+                  <span className={styles.titleGradient}>SGUniDorms</span>
+                </div>
               </a>
             </div>
-            <a
-              href="/"
-              className="hidden md:block text-gray-300 hover:text-white px-3 py-2 rounded-md text-md font-medium"
+            <p
+              className={`hidden md:block text-gray-400 px-3 py-2 rounded-md text-md font-medium cursor-pointer ${
+                activeElement === "nus" ? "text-white" : "hover:text-white"
+              }`}
+              onMouseEnter={() => setActiveElement("nus")}
+              onMouseLeave={() => setActiveElement(null)}
             >
               NUS
-            </a>
-            <a
-              href="/"
-              className="hidden md:block text-gray-300 hover:text-white px-3 py-2 rounded-md text-md font-medium"
-            >
+            </p>
+            <p className="hidden md:block text-gray-400 hover:text-white px-3 py-2 rounded-md text-md font-medium cursor-pointer">
               NTU
-            </a>
-            <a
-              href="/"
-              className="hidden md:block text-gray-300 hover:text-white px-3 py-2 rounded-md text-md font-medium"
-            >
+            </p>
+            <p className="hidden md:block text-gray-400 hover:text-white px-3 py-2 rounded-md text-md font-medium cursor-pointer">
               SMU
-            </a>
+            </p>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <a
