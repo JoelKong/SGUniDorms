ff296d116da1dba92ef34289bb7a1ba945f22d64
Joel Kong <joelkong2001@gmail.com>
2023-05-20 22:47:59 +0800

    feat: added border lines on hovering on nav

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -6,7 +6,6 @@ import { useState } from "react";
 export default function Navigation() {
   const [activeElement, setActiveElement] = useState<String | null>(null);
 
-
   return (
     <nav className="w-full border-b">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
@@ -25,21 +24,48 @@ export default function Navigation() {
                 </div>
               </a>
             </div>
-            <p
-              className={`hidden md:block text-gray-400 px-3 py-2 rounded-md text-md font-medium cursor-pointer ${
-                activeElement === "nus" ? "text-white" : "hover:text-white"
-              }`}
-              onMouseEnter={() => setActiveElement("nus")}
-              onMouseLeave={() => setActiveElement(null)}
-            >
-              NUS
-            </p>
-            <p className="hidden md:block text-gray-400 hover:text-white px-3 py-2 rounded-md text-md font-medium cursor-pointer">
-              NTU
-            </p>
-            <p className="hidden md:block text-gray-400 hover:text-white px-3 py-2 rounded-md text-md font-medium cursor-pointer">
-              SMU
-            </p>
+            <div className="flex flex-col">
+              <p
+                className="hidden md:block text-gray-400 hover:text-white px-3 py-2 rounded-md text-md font-medium cursor-pointer"
+                onMouseEnter={() => setActiveElement("nus")}
+                onMouseLeave={() => setActiveElement(null)}
+              >
+                NUS
+              </p>
+              <div
+                className={`w-full h-0  ${
+                  activeElement === "nus" && "border-b-2"
+                }`}
+              ></div>
+            </div>
+            <div className="flex flex-col">
+              <p
+                className="hidden md:block text-gray-400 hover:text-white px-3 py-2 rounded-md text-md font-medium cursor-pointer"
+                onMouseEnter={() => setActiveElement("ntu")}
+                onMouseLeave={() => setActiveElement(null)}
+              >
+                NTU
+              </p>
+              <div
+                className={`w-full h-0  ${
+                  activeElement === "ntu" && "border-b-2"
+                }`}
+              ></div>
+            </div>
+            <div className="flex flex-col">
+              <p
+                className="hidden md:block text-gray-400 hover:text-white px-3 py-2 rounded-md text-md font-medium cursor-pointer"
+                onMouseEnter={() => setActiveElement("smu")}
+                onMouseLeave={() => setActiveElement(null)}
+              >
+                SMU
+              </p>
+              <div
+                className={`w-full h-0  ${
+                  activeElement === "smu" && "border-b-2"
+                }`}
+              ></div>
+            </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
                 <a
