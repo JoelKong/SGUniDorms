26f7e07bab6785e1954f6129a31d5e2078a63d2f
Joel Kong <joelkong2001@gmail.com>
2023-05-23 16:09:05 +0800

    feat: did up sign up form

--- components/SignInForm.tsx
+++ components/SignInForm.tsx
@@ -1,17 +1,19 @@
-import { useState, useEffect, DOMElement, FormEvent } from "react";
+import { useState, useEffect, FormEvent } from "react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { FcGoogle } from "react-icons/fc";
 
 export default function SignInForm({ setSignIn }: any): JSX.Element {
   const [disable, setDisable] = useState<boolean>(false);
+  const [signUp, setSignUp] = useState<boolean>(false);
 
-  function logIn(e: FormEvent<HTMLFormElement>) {
+  function logInSignUp(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
   }
 
   useEffect(() => {
     const handleEscapeKey = (event: any) => {
       if (event.keyCode === 27) {
+        setSignUp(false);
         setSignIn(false);
       }
     };
@@ -26,20 +28,32 @@ export default function SignInForm({ setSignIn }: any): JSX.Element {
     <section className="w-[100%] h-[100%] fixed top-0 left-0 flex justify-center items-center z-20 backdrop-brightness-50 animate-fade">
       <form
         className="border-2 border-black shadow-inner w-[95vw] md:w-[40vw] h-[80%] rounded-xl bg-gradient-to-br from-[#46458f] to-[#e9b2c08a] flex justify-center items-center relative"
-        onSubmit={(e) => logIn(e)}
+        onSubmit={(e) => logInSignUp(e)}
       >
         <div
           className="absolute text-pink-400 w-8 top-7 right-6 cursor-pointer hover:text-pink-500"
-          onClick={() => setSignIn(false)}
+          onClick={() => {
+            setSignUp(false);
+            setSignIn(false);
+          }}
         >
           <XMarkIcon />
         </div>
         <div className="flex items-center flex-col flex-nowrap w-[80%] h-[90%] ">
           <div className="font-bold text-3xl tracking-wide drop-shadow-2xl">
-            Log In
+            {signUp ? "Sign Up" : "Log In"}
           </div>
+          {signUp && (
+            <input
+              className="mt-10 pl-2 pr-2 w-full md:w-3/4 h-9 rounded-md font-semibold focus:outline-none focus:border-violet-300 focus:border-4 border-4 tracking-wide"
+              placeholder="Display Name"
+              autoFocus
+            ></input>
+          )}
           <input
-            className="mt-10 pl-2 pr-2 w-full md:w-3/4 h-9 rounded-md font-semibold focus:outline-none focus:border-violet-300 focus:border-4 border-4 tracking-wide"
+            className={`${
+              signUp ? "mt-3" : "mt-10"
+            } pl-2 pr-2 w-full md:w-3/4 h-9 rounded-md font-semibold focus:outline-none focus:border-violet-300 focus:border-4 border-4 tracking-wide`}
             placeholder="Email"
             autoFocus
           ></input>
@@ -52,7 +66,7 @@ export default function SignInForm({ setSignIn }: any): JSX.Element {
             className="mt-5 w-full md:w-3/4 h-8 text-white disabled:bg-blue-300 hover:bg-blue-600 bg-blue-500 rounded-md font-medium focus:outline-none focus:border-violet-300 focus:border-2"
             disabled={disable}
           >
-            Log In
+            {signUp ? "Sign Up" : "Log In"}
           </button>
           <div className="flex flex-row w-full md:w-3/4 mt-8 items-center">
             <div className="border-t-2 border-gray-400 w-2/4"></div>
@@ -65,9 +79,12 @@ export default function SignInForm({ setSignIn }: any): JSX.Element {
           </button>
           <div className="w-full md:w-3/4 text-center mt-12 tracking-wide">
             <p>
-              Don't have an account?{" "}
-              <button className="cursor-pointer text-blue-400 font-semibold focus:outline-none focus:border-violet-300 focus:border-2">
-                Sign up
+              {signUp ? "Have an account? " : "Don't have an account? "}
+              <button
+                className="cursor-pointer text-blue-400 font-semibold"
+                onClick={() => setSignUp((prev) => !prev)}
+              >
+                {signUp ? "Log In" : "Sign Up"}
               </button>
             </p>
           </div>
